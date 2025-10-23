b7d6b4c2fe10793bd93e15fd99a0feeb67c1e3c5
valeriaklimenko <klimvaleria3006@gmail.com>
2025-10-23 12:54:26 +0300

    authentication2

--- src/app/Http/Controllers/Auth/AuthController.php
+++ src/app/Http/Controllers/Auth/AuthController.php
@@ -3,95 +3,106 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Http\Requests\ChangePasswordRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdateProfileRequest;
+use App\Services\User\RegisterService;
+use App\Services\User\AuthService;
+use App\Services\User\PasswordService;
+use App\Services\User\ProfileService;
+use Exception;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
+
+
 class AuthController extends Controller
 {
-    public function showLoginForm(): View //returns a view of the login form
+
+    /**
+     * Returns a view of the login form.
+     */
+    public function showLoginForm(): View
     {
         return view('auth.login');
     }
 
-    public function login(Request $request): RedirectResponse //processes the submission of the login form and authorizes the user
+    /**
+     * Processes the submission of the login form and authorizes the user.
+     */
+    public function login(StorePostRequest $request, AuthService $authService): RedirectResponse
     {
-        $data = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-        if (Auth::attempt($data)) {
+        if ($authService->login($request->validated())) {
             return redirect()->intended(route('profile'));
         }
-        return back()->withErrors(['email' => 'не верные учетные данные']);
+        return redirect()->back()->withErrors(['email' => 'Credentials are not correct']);
     }
 
-    public function logout(): RedirectResponse //ends user's authentication and redirect to the login page
+    /**
+     * Ends user's authentication and redirect to the login page.
+     */
+    public function logout(AuthService $authService): RedirectResponse
     {
-        auth()->logout();
-
+       $authService->logout();
         return redirect()->route('login');
     }
 
-    public function showRegisterForm(): View //returns a view of the register form
+    /**
+     * Returns a view of the register form.
+     */
+    public function showRegisterForm(): View
     {
         return view('auth.register');
     }
 
-    public function register(Request $request): RedirectResponse //creates a new user account in the system and redirects to the login page
+    /**
+     * Creates a new user account in the system and redirects to the login page.
+     */
+    public function register(RegisterRequest $request, RegisterService $registerService): RedirectResponse
     {
-        $data = $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|string|email|unique:users',
-            'password' => 'required|string|min:8|confirmed'
-        ]);
+        $registerService->register($request->validated());
 
-        $data['password'] = Hash::make($data['password']);
-        User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => $data['password']
-        ]);
         return redirect()->route('login')->with('status', 'success');
     }
 
-    public function profile(): View //returns a view of the profile form
+    /**
+     * returns a view of the profile form.
+     */
+    public function profile(): View
     {
         $user = Auth::user();
         return view('auth.profile', compact('user'));
     }
 
-    public function updateProfile(Request $request): RedirectResponse //handles updating user data (name and email)
+    /**
+     * handles updating user data (name and email).
+     */
+    public function updateProfile(UpdateProfileRequest $request, ProfileService $profileService): RedirectResponse
     {
-        $data = $request->validate([
-            'name' => 'string',
-            'email' => 'string|email|unique:users,email,' . Auth::id(),
-        ]);
         $user = Auth::user();
-        $user->update($data);
+        $profileService->update($user, $request->validated());
         return redirect()->route('profile');
     }
 
-    public function showChangePasswordForm(): View //returns a view of the change-password
+    /**
+     * returns a view of the change-password.
+     */
+    public function showChangePasswordForm(): View
     {
         return view('auth.change-password');
     }
 
-    public function changePassword(Request $request): RedirectResponse //allows the user to securely change password
+    /**
+     * allows the user to securely change password.
+     */
+    public function changePassword(ChangePasswordRequest $request, PasswordService $passwordService): RedirectResponse
     {
-        $data = $request->validate([
-            'current_password' => 'required|string|min:8',
-            'new_password' => 'required|string|min:8|confirmed',
-        ]);
-        $user = Auth::user();
-        if (!Hash::check($data['current_password'], $user->password)) {
-            return back()->withErrors(['current_password' => 'the current password is incorrect!']);
-        }
-        $user->password = Hash::make($data['new_password']);
-        $user->save();
-        return redirect()->route('profile')->with('status', 'Your password has been changed!');
+        $user = auth()->user();
+       $passwordService->changePassword($user, $request->validated());
+        return redirect()->route('profile')->with('status', 'Password successfully changed!');
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -7,12 +7,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [AuthController::class,'showLoginForm'])->name('loginForm');
-Route::post('/login',[AuthController::class, 'login'])->name('login');
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-Route::get('/register', [AuthController::class,'showRegisterForm'])->name('registerForm');
-Route::post('/register',[AuthController::class, 'register'])->name('register');
-Route::get('/profile', [AuthController::class,'profile'])->name('profile');
-Route::patch('/profile', [AuthController::class,'updateProfile'])->name('updateProfile');
-Route::get('/change-password', [AuthController::class,'showChangePasswordForm'])->name('changePasswordForm');
-Route::post('/change-password',[AuthController::class, 'changePassword'])->name('changePassword');
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/login', 'showLoginForm')->name('loginForm');
+    Route::post('/login', 'login')->name('login');
+    Route::get('/logout', 'logout')->name('logout');
+    Route::post('/logout', 'logout')->name('logout');
+    Route::get('/register', 'showRegisterForm')->name('registerForm');
+    Route::post('/register', 'register')->name('register');
+    Route::get('/profile', 'profile')->name('profile');
+    Route::patch('/profile', 'updateProfile')->name('updateProfile');
+    Route::get('/change-password', 'showChangePasswordForm')->name('changePasswordForm');
+    Route::post('/change-password', 'changePassword')->name('changePassword');
+});
+
+
